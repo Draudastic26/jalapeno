e3566a6b2940284ed6e5f59549b4e08399d4fb61
Eduard Reger <eduard.reger@gmail.com>
2022-03-21 15:52:00 +0100

    Finish span fill

--- src/main/kotlin/com/draudastic/algo/SpanFilling.kt
+++ src/main/kotlin/com/draudastic/algo/SpanFilling.kt
@@ -1,69 +1,67 @@
 package com.draudastic.algo
 
 import com.draudastic.battlensake.BoardState
+import com.draudastic.models.Position
 
 data class SpanFillSet(val x1: Int, val x2: Int, val y: Int, val dy: Int)
 
 object SpanFilling {
-/*
- fn fill(x, y):
-  if not Inside(x, y) then return
-  let s = new empty queue or stack
-  Add (x, x, y, 1) to s
-  Add (x, x, y - 1, -1) to s
-  while s is not empty:
-    Remove an (x1, x2, y, dy) from s
-    let x = x1
 
-    if Inside(x, y):
-      while Inside(x - 1, y):
-        Set(x - 1, y)
-        x = x - 1
-    if x < x1:
-      Add (x, x1-1, y-dy, -dy) to s
-    while x1 <= x2:
-      while Inside(x1, y):
-        Set(x1, y)
-        x1 = x1 + 1
-      Add (x, x1 - 1, y+dy, dy) to s
-      if x1 - 1 > x2:
-        Add (x2 + 1, x1 - 1, y-dy, -dy) to s
-      x1 = x1 + 1
-      while x1 < x2 and not Inside(x1, y):
-        x1 = x1 + 1
-      x = x1
-*/
-
+    private val setList = mutableSetOf<Position>()
 
     // Returns the number of "filled" pixels
-    fun BoardState.fill(x: Int, y: Int): Int {
-        val filledPixel = 0
+    fun BoardState.fill(startX: Int, startY: Int): Int {
+        setList.clear()
 
-        if (!this.inside(x, y)) return 0
+        if (!this.inside(startX, startY)) return 0
 
         val s = mutableListOf<SpanFillSet>()
-        s.add(SpanFillSet(x, x, y, 1))
-        s.add(SpanFillSet(x, x, y - 1, -1))
+        s.add(SpanFillSet(startX, startX, startY, 1))
+        s.add(SpanFillSet(startX, startX, startY - 1, -1))
 
         while (s.isNotEmpty()) {
-            val curr = s.random()
-            val x = curr.x1
-            if(this.inside(x, y)) {
-                while(this.inside(x - 1, y)) {
-                    this.set(x -1, y)
-                    x = x -1
+            val cur = s.random()
+            var x1 = cur.x1
+            val x2 = cur.x2
+            val y = cur.y
+            val dy = cur.dy
+
+            var x = cur.x1
+            if (this.inside(x, y)) {
+                while (this.inside(x - 1, y)) {
+                    set(x - 1, y)
+                    x = x - 1
                 }
+            }
+            if (x < x1) {
+                s.add(SpanFillSet(x, x1 - 1, y - dy, dy * -1))
+            }
+            while (x1 <= x2) {
+                while (this.inside(x1, y)) {
+                    set(x1, y)
+                    x1 = x1 + 1
+                }
+                s.add(SpanFillSet(x, x1 - 1, y + dy, dy))
+                if (x1 - 1 > x2) {
+                    s.add(SpanFillSet(x2 + 1, x1 - 1, y - dy, dy * -1))
+                }
+                x1 = x1 * 1
+                while(x1 < x2 && !this.inside(x1, y)) {
+                    x1 = x1 + 1
+                }
+                x = x1
             }
         }
 
-        return filledPixel
+        return setList.count()
     }
 
     private fun BoardState.inside(x: Int, y: Int): Boolean {
-
+        val pos = Position(x, y)
+        return pos !in this.avoidPositions && pos !in setList
     }
 
-    private fun BoardState.set(x: Int, y: Int) {
-
+    private fun set(x: Int, y: Int) {
+        setList.add(Position(x, y))
     }
 }

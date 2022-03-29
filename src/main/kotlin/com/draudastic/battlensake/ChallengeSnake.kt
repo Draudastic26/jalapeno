bc5b802a5283afb2104641e4b2c549527a0bb0e0
Eduard Reger <eduard.reger@gmail.com>
2022-03-29 16:38:38 +0200

    Remove closed area from challenge snake

--- src/main/kotlin/com/draudastic/battlensake/ChallengeSnake.kt
+++ src/main/kotlin/com/draudastic/battlensake/ChallengeSnake.kt
@@ -1,6 +1,5 @@
 package com.draudastic.battlensake
 
-import com.draudastic.algo.FloodFill.removeClosedAreas
 import com.draudastic.models.Move
 import com.draudastic.models.MoveRequest
 import mu.KotlinLogging
@@ -12,11 +11,8 @@ class ChallengeSnake(override val info: Info) : BattleSnake() {
     override fun decideMove(moveRequest: MoveRequest): Move {
         var possibleMoves = action.getPossibleMoves(state.you.head, state.avoidPositions)
 
-        val possibleMovesWithoutClosedAreaMove = state.removeClosedAreas(possibleMoves)
-        if (possibleMovesWithoutClosedAreaMove.isNotEmpty()) possibleMoves = possibleMovesWithoutClosedAreaMove
-
         var target = state.you.body.last().position
-        if (state.you.health < 10) target = state.getClosestFood()?.position ?: target
+        if (state.you.health < 5) target = state.getClosestFood()?.position ?: target
 
         val nextMove = action.moveTowards(state.you.head, target, possibleMoves)
 
